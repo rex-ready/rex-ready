9f4474bf27062c51eaad7bbb85e641e77f9afe77
Peter Prince <pete.c.prince@gmail.com>
2014-11-23 11:44:11 +0000

    Fix batch compile scripts
    Added charts4j to classpath.

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 javac -classpath . com/botbox/util/*.java
 javac -classpath . se/sics/tac/aw/*.java
 javac -classpath . se/sics/tac/util/*.java
-javac -classpath . rexready/*.java
+javac -classpath .;lib/ rexready/*.java
 jar cfm tacagent.jar AWManifest.txt com/botbox/util/*.class se/sics/tac/aw/*.class se/sics/tac/util/*.class
