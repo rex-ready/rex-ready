fc2bfb23dfcccfe8b1e9c3f998d64cb67c961e5c
James Robinson <admin@robinjam.net>
2014-11-28 16:45:45 +0000

    Remove unused classes from earlier in development

--- src/main/java/rexready/Client.java
+++ src/main/java/rexready/Client.java
@@ -8,25 +8,6 @@ public class Client {
 	preferences = new Preferences(arrival, departure, hotel_value, e1, e2, e3);
     }
 
-    public int travelPenalty(TravelPackage travelPackage) {
-	return 100 * (Math.abs(travelPackage.getArrivalDate() - preferences.getArrival()) + Math.abs(travelPackage.getDepartureDate() - preferences.getDeparture()));
-    }
-
-    public int hotelBonus(TravelPackage travelPackage) {
-	return travelPackage.includesTampaTowers() ? preferences.getHotelValue() : 0;
-    }
-
-    public int funBonus(EntertainmentPackage entertainmentPackage) {
-	boolean[] includes = entertainmentPackage.includesEntertainmentTypes();
-	return (includes[0] ? preferences.getE1Value() : 0)
-		+ (includes[1] ? preferences.getE2Value() : 0)
-		+ (includes[2] ? preferences.getE2Value() : 0);
-    }
-
-    public int utility(TravelPackage travelPackage, EntertainmentPackage entertainmentPackage) {
-	return 1000 - travelPenalty(travelPackage) + hotelBonus(travelPackage) + funBonus(entertainmentPackage);
-    }
-
     public int getMaxValue() {
 	return getMaxTravelValue() + getMaxEntertainmentValue();
     }
