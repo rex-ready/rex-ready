417cd55078e2ceeb10698f4e046d39ae93f0610c
Peter Prince <pete.c.prince@gmail.com>
2014-12-05 13:38:40 +0000

    Add file output to main agent

--- src/main/java/rexready/RexReady.java
+++ src/main/java/rexready/RexReady.java
@@ -1,5 +1,31 @@
 package rexready;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.googlecode.charts4j.AxisLabels;
+import com.googlecode.charts4j.AxisLabelsFactory;
+import com.googlecode.charts4j.AxisStyle;
+import com.googlecode.charts4j.AxisTextAlignment;
+import com.googlecode.charts4j.Color;
+import com.googlecode.charts4j.Data;
+import com.googlecode.charts4j.DataUtil;
+import com.googlecode.charts4j.Fills;
+import com.googlecode.charts4j.GCharts;
+import com.googlecode.charts4j.Line;
+import com.googlecode.charts4j.LineChart;
+import com.googlecode.charts4j.LinearGradientFill;
+import com.googlecode.charts4j.Plots;
+
 import se.sics.tac.aw.AgentImpl;
 import se.sics.tac.aw.Bid;
 import se.sics.tac.aw.Quote;
@@ -12,10 +38,24 @@ public class RexReady extends AgentImpl {
 	private HotelPricePredictor hotelPredictor = new HotelPricePredictor();
 	private EntertainmentPricePredictor entertainmentPredictor = new EntertainmentPricePredictor();
 	private Optimiser optimiser = new Optimiser();
+	
+	private Map<String, List<Float>> askPrices = new HashMap<String, List<Float>>();
+	private Map<String, List<Float>> predictedMinimumFlightPrices = new HashMap<String, List<Float>>();
+	private Map<String, List<Float>> bidPrices = new HashMap<String, List<Float>>();
+	
+	private BufferedWriter bwHotels;
 
 	@Override
 	protected void init(ArgEnumerator args) {
 		System.out.println("init");
+		
+		for (int i = 0; i < TACAgent.getAuctionNo(); i++) {
+			askPrices.put(TACAgent.getAuctionTypeAsString(i), new ArrayList<Float>());
+			bidPrices.put(TACAgent.getAuctionTypeAsString(i), new ArrayList<Float>());
+			if (i < 8) {
+				predictedMinimumFlightPrices.put(TACAgent.getAuctionTypeAsString(i), new ArrayList<Float>());
+			}
+		}
 	}
 
 	@Override
@@ -38,6 +78,56 @@ public class RexReady extends AgentImpl {
 	@Override
 	public void gameStopped() {
 		System.out.println("gameStopped");
+		
+		try {
+			DateFormat dateFormat = new SimpleDateFormat("ddMMyyyyHHmmss");
+			Date date = new Date();
+
+			File chartsFile = new File("data\\" + dateFormat.format(date) + "charts.txt");
+			if (!chartsFile.exists()) {
+				chartsFile.createNewFile();
+			}
+			File priceFile = new File("data\\" + dateFormat.format(date) + "prices.txt");
+			if (!priceFile.exists()) {
+				priceFile.createNewFile();
+			}
+
+			FileWriter fwCharts = new FileWriter(chartsFile.getAbsoluteFile());
+			BufferedWriter bwCharts = new BufferedWriter(fwCharts);
+			FileWriter fwPrices = new FileWriter(priceFile.getAbsoluteFile());
+			BufferedWriter bwPrices = new BufferedWriter(fwPrices);
+			for (int i = 0; i < TACAgent.getAuctionNo(); i++) {
+				List<Float> askPriceList = askPrices.get(TACAgent.getAuctionTypeAsString(i));
+				List<Float> bidPriceList = bidPrices.get(TACAgent.getAuctionTypeAsString(i));
+				if (TACAgent.getAuctionCategory(i) == TACAgent.CAT_FLIGHT) {
+					int flightDataPointsPast = (int) (agent.getGameTime() / 10000);
+					String url = createPredictionChart(i, predictedMinimumFlightPrices.get(TACAgent.getAuctionTypeAsString(i)), 54, flightDataPointsPast);
+					bwCharts.write(url);
+				} else {
+					bwCharts.write(createChart(i));
+				}
+				bwCharts.newLine();
+
+				bwPrices.write("Auction " + i);
+				bwPrices.newLine();
+				for (Float f : askPriceList) {
+					bwPrices.write(f + " ");
+				}
+				if (TACAgent.getAuctionCategory(i) == TACAgent.CAT_ENTERTAINMENT) {
+					bwPrices.newLine();
+					for (Float b : bidPriceList) {
+						bwPrices.write(b + " ");
+					}
+				}
+
+				bwPrices.newLine();
+			}
+			bwCharts.close();
+			bwPrices.close();
+			bwHotels.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
@@ -47,11 +137,17 @@ public class RexReady extends AgentImpl {
 		
 		int t = (int) Math.ceil(agent.getGameTime() / 10000.f);
 		
+		askPrices.get(TACAgent.getAuctionTypeAsString(quote.getAuction())).add(quote.getAskPrice());
+		bidPrices.get(TACAgent.getAuctionTypeAsString(quote.getAuction())).add(quote.getBidPrice());
+				
 		switch (TACAgent.getAuctionCategory(quote.getAuction())) {
 		case TACAgent.CAT_FLIGHT:
 			flightPredictor.previousPrices[quote.getAuction()] = flightPredictor.currentPrices[quote.getAuction()];
 			flightPredictor.currentPrices[quote.getAuction()] = quote.getAskPrice();
 			flightPredictor.updateProbabilityDistribution(quote.getAuction(), t);
+			
+			int auctionID = quote.getAuction();
+			predictedMinimumFlightPrices.get(TACAgent.getAuctionTypeAsString(auctionID)).add(flightPredictor.getProbableMinimumPrice(auctionID, t, agent.getQuote(auctionID).getAskPrice()));
 			break;
 		case TACAgent.CAT_HOTEL:
 			hotelPredictor.previousPrices[quote.getAuction() - 8] = hotelPredictor.currentPrices[quote.getAuction() - 8];
@@ -65,6 +161,27 @@ public class RexReady extends AgentImpl {
 		
 		if (quote.getAuction() == 7) {
 			hotelPredictor.updateDeltas(t);
+			
+			String firstString = String.format("First: %f, %f, %f Counterpart: %f, %f, %f\n" , hotelPredictor.previousPrices[0], hotelPredictor.currentPrices[0], hotelPredictor.deltas[0], hotelPredictor.previousPrices[4], hotelPredictor.currentPrices[4], hotelPredictor.deltas[4]);
+			String secondString = String.format("Second: %f, %f, %f Counterpart: %f, %f, %f\n" , hotelPredictor.previousPrices[1], hotelPredictor.currentPrices[1], hotelPredictor.deltas[1], hotelPredictor.previousPrices[5], hotelPredictor.currentPrices[5], hotelPredictor.deltas[5]);
+			String thirdString = String.format("Third: %f, %f, %f Counterpart: %f, %f, %f\n" , hotelPredictor.previousPrices[2], hotelPredictor.currentPrices[2], hotelPredictor.deltas[2], hotelPredictor.previousPrices[6], hotelPredictor.currentPrices[6], hotelPredictor.deltas[6]);
+			String fourthString = String.format("Fourth: %f, %f, %f Counterpart: %f, %f, %f\n\n" , hotelPredictor.previousPrices[3], hotelPredictor.currentPrices[3], hotelPredictor.deltas[3], hotelPredictor.previousPrices[7], hotelPredictor.currentPrices[7], hotelPredictor.deltas[7]);
+			try {
+				bwHotels.write(firstString);
+				bwHotels.newLine();
+				bwHotels.write(secondString);
+				bwHotels.newLine();
+				bwHotels.write(thirdString);
+				bwHotels.newLine();
+				bwHotels.write(fourthString);
+				bwHotels.newLine();
+				bwHotels.write("-----");
+				bwHotels.newLine();
+				bwHotels.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
 			entertainmentPredictor.updateDeltas(t);
 			PriceData priceData = new PriceData();
 			for (int i = 0; i < TACAgent.getAuctionNo(); ++i) {
@@ -196,6 +313,112 @@ public class RexReady extends AgentImpl {
 			int hotelID = auction - 8;
 			hotelPredictor.closed[hotelID] = (int) Math.ceil(agent.getGameTime() / 10000.f);
 		}
+	}
+	
+	private String createChart(int auctionID) {
+		String typeString = TACAgent.getAuctionTypeAsString(auctionID);
+
+		List<Float> priceValues = new ArrayList<Float>(askPrices.get(typeString));
+
+		return createChart(priceValues, typeString);
+	}
+
+	private String createChart(List<Float> priceValues, String typeString) {
+		Data data = DataUtil.scaleWithinRange(0, 1000, priceValues);
+		Line line = Plots.newLine(data, Color.GREEN);
+
+		LineChart chart = GCharts.newLineChart(line);
+		chart.setSize(550, 400);
+		chart.setTitle(typeString, Color.WHITE, 14);
+		chart.setGrid(10, 10, 3, 2);
+
+		// Defining axis info and styles
+		chart.addXAxisLabels(AxisLabelsFactory.newNumericRangeAxisLabels(0, priceValues.size()));
+		chart.addYAxisLabels(AxisLabelsFactory.newNumericRangeAxisLabels(0, 1000));
+
+		AxisLabels xAxisLabel = AxisLabelsFactory.newAxisLabels("Price Changes", 50.0);
+		xAxisLabel.setAxisStyle(AxisStyle.newAxisStyle(Color.WHITE, 14, AxisTextAlignment.CENTER));
+
+		AxisLabels yAxisLabel = AxisLabelsFactory.newAxisLabels("Prices", 50.0);
+		yAxisLabel.setAxisStyle(AxisStyle.newAxisStyle(Color.WHITE, 14, AxisTextAlignment.CENTER));
+
+		// Adding axis info to chart.
+		chart.addXAxisLabels(xAxisLabel);
+		chart.addYAxisLabels(yAxisLabel);
+
+		// Defining background and chart fills.
+		chart.setBackgroundFill(Fills.newSolidFill(Color.newColor("1F1D1D")));
+		LinearGradientFill fill = Fills.newLinearGradientFill(0, Color.newColor("363433"), 100);
+		fill.addColorAndOffset(Color.newColor("2E2B2A"), 0);
+		chart.setAreaFill(fill);
+		String url = chart.toURLString();
+
+		return url;
+	}
+
+	private String createPredictionChart(int auctionID, List<Float> predictions, int maxDataPointCount, int pastPointCount) {
+		String typeString = TACAgent.getAuctionTypeAsString(auctionID);
+		
+		int missedPointCount = (pastPointCount - askPrices.get(typeString).size());
+		
+		List<Float> priceValues = new ArrayList<Float>();
+		float firstPriceValue = askPrices.get(typeString).get(0);
+		for(int i = 0; i <= missedPointCount; i++){
+			priceValues.add(firstPriceValue);
+		}
+		List<Float> predictionList = new ArrayList<Float>();
+		for(int i = 0; i <= missedPointCount; i++){
+			predictionList.add(firstPriceValue);
+		}
+				
+		priceValues.addAll(askPrices.get(typeString));
+		float lastPriceValue = priceValues.get(priceValues.size() - 1);
+		for (int i = priceValues.size(); i < maxDataPointCount; i++) {
+			priceValues.add(lastPriceValue);
+		}
+		predictionList.addAll(predictions);
+		float lastPredictionValue = predictionList.get(predictionList.size() - 1);
+		for (int i = predictionList.size(); i < maxDataPointCount; i++) {
+			predictionList.add(lastPredictionValue);
+		}
+
+		Data priceData = DataUtil.scaleWithinRange(0, 1000, priceValues);
+		Data predictionData = DataUtil.scaleWithinRange(0, 1000, predictionList);
+		Line priceLine = Plots.newLine(priceData, Color.GREEN);
+		Line predictionLine = Plots.newLine(predictionData, Color.RED);
+
+		LineChart chart = GCharts.newLineChart(priceLine, predictionLine);
+		chart.setSize(550, 400);
+		chart.setTitle(typeString, Color.WHITE, 14);
+		if(missedPointCount > 0) {
+			double r = (1.0 * missedPointCount/maxDataPointCount);
+			chart.addVerticalRangeMarker(0, 100.0*r, Color.DARKSLATEGRAY);
+		}
+		double gr = 100.0 * (5.0 / maxDataPointCount);
+		chart.setGrid(gr, 10.0, 3, 2);
+		
+		// Defining axis info and styles
+		chart.addXAxisLabels(AxisLabelsFactory.newNumericRangeAxisLabels(0, priceValues.size()));
+		chart.addYAxisLabels(AxisLabelsFactory.newNumericRangeAxisLabels(0, 1000));
+
+		AxisLabels xAxisLabel = AxisLabelsFactory.newAxisLabels("Price Changes", 50.0);
+		xAxisLabel.setAxisStyle(AxisStyle.newAxisStyle(Color.WHITE, 14, AxisTextAlignment.CENTER));
+
+		AxisLabels yAxisLabel = AxisLabelsFactory.newAxisLabels("Prices", 50.0);
+		yAxisLabel.setAxisStyle(AxisStyle.newAxisStyle(Color.WHITE, 14, AxisTextAlignment.CENTER));
+
+		// Adding axis info to chart.
+		chart.addXAxisLabels(xAxisLabel);
+		chart.addYAxisLabels(yAxisLabel);
+
+		// Defining background and chart fills.
+		chart.setBackgroundFill(Fills.newSolidFill(Color.newColor("1F1D1D")));
+		LinearGradientFill fill = Fills.newLinearGradientFill(0, Color.newColor("363433"), 100);
+		fill.addColorAndOffset(Color.newColor("2E2B2A"), 0);
+		chart.setAreaFill(fill);
+		String url = chart.toURLString();
+
+		return url;
 	}
 	
 }
